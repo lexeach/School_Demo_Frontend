c67e44730ed483ae762f8cb742f1e5a4a88c25ce
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-08 21:22:05 +0530

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@
 // export const BaseURL = 'http://localhost:5173' // locals URL
 
 // Production  ( Un comment both two line for the production)
-  export const BaseURL = 'https://test.exowa.click' // live URL
+  export const BaseURL = 'https://exowa-school-demo.netlify.app' // live URL
  // export const apiUrl = 'https://exowa-backend.onrender.com' // live URL
     export const apiUrl = 'https://school-demo-backend.onrender.com'
 
