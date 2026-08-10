6f17495558d5accfd561d0dbbd7ca48eca9f8d83
lexeach <80155340+lexeach@users.noreply.github.com>
2026-08-10 20:22:43 +0530

    Update url-constants.ts

--- src/config/url-constants.ts
+++ src/config/url-constants.ts
@@ -194,3 +194,4 @@ export const API_GET_LEARNING_VERIFICATION = (
 
 export const API_SUBMIT_LEARNING_VERIFICATION =
   "/api/learning-verification/submit";
+export const API_BULK_UPLOAD_USERS = "/api/users/bulk-upload";
